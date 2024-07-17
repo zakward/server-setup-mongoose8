7f8894e23a9e3995d4a19132a63029ca5340e525
Zak Ward <zakward85@gmail.com>
2024-07-17 16:47:05 -0500

    code removal for workshop

--- models/user.js
+++ models/user.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 const bcrypt = require("bcrypt")
 
+
+
 const userSchema = new Schema({
     username: {
         type: String,
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,49 +5,11 @@ const jwt = require("jsonwebtoken")
 
 
 //sign up route mongoose 8
-// async await
-// try catch statement
-authRouter.post("/signup", async (req, res, next) => {
-    try {
-        const user = await User.findOne({ username: req.body.username })
-        if (user) {
-            res.status(403)
-            return next(new Error("Username is already taken! Please try again."))
-        }
-        const newUser = new User(req.body)
-        const savedUser = await newUser.save()
-        const token = jwt.sign(savedUser.toObject(), process.env.SECRET)
-        return res.status(201).send({ user: savedUser.withoutPassword(), token })
 
-    } catch (error) {
-        res.status(500)
-        return next(error)
-    }
-})
+
+
 
 //log in route mongoose 8
-
-authRouter.post("/login", async (req, res, next) => {
-    try {
-        const user = await User.findOne({ username: req.body.username })
-        if (!user) {
-            res.status(403)
-            return next(new Error("Username or Password is incorrect, please try again!"))
-        }
-        const isMatch = await user.checkPassword((req.body.password))
-        if (!isMatch) {
-            return res.status(403).send(new Error("Username or Password is incorrect, please try again!"))
-        }
-        const token = jwt.sign(user.toObject(), process.env.SECRET)
-        return res.status(201).send({ user: user.withoutPassword(), token })
-
-    } catch (error) {
-        res.status(500)
-        return next(error)
-    }
-})
-
-
 
 
 
